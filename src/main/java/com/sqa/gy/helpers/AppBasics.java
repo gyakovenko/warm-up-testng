71f5d6052bedab700f6fb956ff2a95668e2d4775
Galina Yakovenko <gyakovenko@gmail.com>
2017-01-19 18:28:36 -0800

    Update AppBasics, Create NumberNotInRangeExceptions

--- src/main/java/com/sqa/gy/helpers/AppBasics.java
+++ src/main/java/com/sqa/gy/helpers/AppBasics.java
@@ -9,6 +9,8 @@ package com.sqa.gy.helpers;
 
 import java.util.*;
 
+import com.sqa.gy.helpers.exceptions.*;
+
 public class AppBasics {
 
 	static Scanner scanner = new Scanner(System.in);
@@ -42,10 +44,56 @@ public class AppBasics {
 		return scanner.nextLine();
 	}
 
+	public static int requestIntFromUser(String question) {
+		return requestIntFromUser(question, 0, 0);
+	}
+
+	public static int requestIntFromUser(String question, int min, int max) {
+		String input;
+		int result = 0;
+		boolean isNotValid = true;
+		while (isNotValid) {
+			try {
+				System.out.print(question + " ");
+				input = scanner.nextLine();
+				result = Integer.parseInt(input);
+				if (min != 0 && max != 0) {
+					if (result < max && result > min) {
+						isNotValid = false;
+					} else {
+						throw new NumberNotInRangeException();
+					}
+				}
+			} catch (NumberFormatException e) {
+				System.out.println("You have not entered a correct number value.");
+			} catch (NumberNotInRangeException e) {
+				System.out.println("You have not entered a number within the range of " + min + " and " + max + ".");
+			}
+		}
+		return result;
+	}
+
 	public static String requestUserInfo(String prompt) {
 		System.out.print(prompt + " ");
 		String response = scanner.nextLine();
 		return response;
+	}
+
+	public static int requestUserInt(String question) {
+		String input;
+		int result = 0;
+		boolean isNotValid = true;
+		while (isNotValid) {
+			try {
+				System.out.print(question + " ");
+				String response = scanner.nextLine();
+				result = Integer.parseInt(response);
+				isNotValid = false;
+			} catch (NumberFormatException e) {
+				System.out.print("The value you have entered is invalid. ");
+			}
+		}
+		return result;
 	}
 
 	public static boolean requestYesorNo(String prompt) {
---
+++ src/main/java/com/sqa/gy/helpers/exceptions/NumberNotInRangeException.java
@@ -0,0 +1,19 @@
+/**
+ *   File Name: NumberNotInRangeException.java<br>
+ *
+ *   Yakovenko, Galina<br>
+ *   Created: Jan 18, 2017
+ *
+ */
+
+package com.sqa.gy.helpers.exceptions;
+
+/**
+ * NumberNotInRangeException
+ *
+ * @author Yakovenko, Galina
+ *
+ */
+
+public class NumberNotInRangeException extends Exception {
+}
