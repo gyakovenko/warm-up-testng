f99783ce1937f9000b9e87040212146cb75d2fac
Galina Yakovenko <gyakovenko@gmail.com>
2017-01-22 20:17:28 -0800

    Update AppBasics

--- src/main/java/com/sqa/gy/helpers/AppBasics.java
+++ src/main/java/com/sqa/gy/helpers/AppBasics.java
@@ -38,6 +38,11 @@ public class AppBasics {
 		System.out.println("Bye for now, " + nameOfUser + "!");
 	}
 
+	public static String greetNoUserName(String appName) {
+		System.out.println("Hello there and welcome to the " + appName + " app.");
+		return scanner.nextLine();
+	}
+
 	public static String greetUser(String appName) {
 		System.out.println("Hello and welcome to the " + appName + " app.");
 		System.out.print("What shall I call you? ");
@@ -111,11 +116,17 @@ public class AppBasics {
 		System.out.print("\n");
 	}
 
-	public static int requestIntFromUser(String question) {
-		return requestIntFromUser(question, 0, 0);
+	public static String requestUserInfo(String prompt) {
+		System.out.print(prompt + " ");
+		String response = scanner.nextLine();
+		return response;
 	}
 
-	public static int requestIntFromUser(String question, int min, int max) {
+	public static int requestUserInt(String question) {
+		return requestUserInt(question, 0, 0);
+	}
+
+	public static int requestUserInt(String question, int min, int max) {
 		String input;
 		int result = 0;
 		boolean isNotValid = true;
@@ -137,29 +148,6 @@ public class AppBasics {
 				System.out.println("You have not entered a number within the range of " + min + " and " + max + ".");
 			}
 			isNotValid = false;
-		}
-		return result;
-	}
-
-	public static String requestUserInfo(String prompt) {
-		System.out.print(prompt + " ");
-		String response = scanner.nextLine();
-		return response;
-	}
-
-	public static int requestUserInt(String question) {
-		String input;
-		int result = 0;
-		boolean isNotValid = true;
-		while (isNotValid) {
-			try {
-				System.out.print(question + " ");
-				String response = scanner.nextLine();
-				result = Integer.parseInt(response);
-				isNotValid = false;
-			} catch (NumberFormatException e) {
-				System.out.print("The value you have entered is invalid. ");
-			}
 		}
 		return result;
 	}
